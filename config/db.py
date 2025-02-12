ab3298cb52ca7ca35edaa30822ea783484cca965
Akash Adhyapak <105267317+akashhhshsh@users.noreply.github.com>
2025-02-13 03:46:37 +0530

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
-MONGO_URI = "mongodb+srv://akash:%40lohamorA@cluster0.ntiw3.mongodb.net/"
+MONGO_URI = "Insert your own MongoDB URI"
 
 conn = MongoClient(MONGO_URI)
